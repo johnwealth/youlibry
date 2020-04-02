eab0a336b6200916f74f90f4144ba3fbcc8f4c5f
dotun peter <oyelakindotun@gmail.com>
2020-04-02 21:52:14 +0100

    refactoring

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from blueprints.core import bp as bp_core
-
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'My_Top_Secert_Key'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
     # Blueprint
 
