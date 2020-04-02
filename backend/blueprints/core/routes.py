c8c536eefe50189f5bb6e6bd961df3024fcb0074
johnwealth <johnwealth.ise@gmail.com>
2020-04-02 19:04:52 +0100

    template added second commit

--- backend/blueprints/core/routes.py
+++ backend/blueprints/core/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from blueprints.core.bp import bp
 
+
 @bp.route('/')
-def home():
-    return render_template('hello.html')
+@bp.route('/<name>')
+def home(name = None):
+    return render_template('hello.html', name=name)
